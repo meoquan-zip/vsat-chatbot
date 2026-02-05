3f25b634a9e54788bfa13ebd848b16a6ff46d18e
Nguyen Meo Quan <214677494+meoquan-zip@users.noreply.github.com>
2026-02-05 16:14:04 +0700

    refactor templates directory

--- app/utils/template.py
+++ app/utils/template.py
@@ -1,7 +1,7 @@
 import os
 
 
-def load_templates_as_env_vars(template_dir: str = "app/templates") -> None:
+def load_templates_as_env_vars(template_dir: str = "templates") -> None:
     """Load all template files in the specified directory as environment variables."""
     for fn in os.listdir(template_dir):
         fpath = os.path.join(template_dir, fn)
